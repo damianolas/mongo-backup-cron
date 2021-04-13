8a97a082afd6a1e689f208b4e043d22f5966b677
damianolas <damiano.lasciarrea@gmail.com>
2021-04-13 11:27:48 +0200

    Create mongo-backup.sh

---
+++ mongo-backup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+  
+set -e
+
+S3="<NOME-BUCKET>"
+BACKUP_NAME=$(date +%y%m%d_%H%M%S).gz
+DB="<NOME-DB>"
+ENV="<SOTTOCARTELLA-BUCKET>"
+
+date
+
+echo "Dumping MongoDB $DB database to compressed archive"
+mongodump --db $DB --excludeCollection "Logs" --archive=$HOME/mongobackup/<NOME>.gz --gzip
+
+echo "Copying compressed archive to S3 bucket: $S3"
+aws s3 cp $HOME/mongobackup/<NOME>.gz s3://$S3/$ENV/$BACKUP_NAME
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                                                                                                                              
+~                                                                                  
